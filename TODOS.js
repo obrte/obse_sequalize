e1865f3c2eafbea0525b5130562fd2b976a673ba
Omar Bracamonte <omar.bracamonte@gmail.com>
2018-07-31 16:08:30 -0700

    TODOs

---
+++ TODOS.js
@@ -0,0 +1,7 @@
+/**
+ * TODO: Trimer cada dato del body parse y convertirlo a MAYUSCULAS.
+ * ! exepto el email.
+ * TODO: Hacer todo lo del login y manejo de usuarios.
+ * TODO: Todas las rutas deben ser protegidas, con excepción del login.
+ * TODO: Hacer un middleware para decodificar el token que posee el usuario al momento de autenticar (JWT).
+ **/
