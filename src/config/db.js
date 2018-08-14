dbf39ecd6c27b00375c0ece8f49bedfbe233841b
Omar Bracamonte <omar.bracamonte@gmail.com>
2018-08-14 14:15:40 -0700

    informes

--- src/config/db.js
+++ src/config/db.js
@@ -154,5 +154,9 @@ db.informes.belongsTo(db.catFondos, {
 	foreignKey: 'idFondo',
 	as: 'fondo'
 })
+db.informes.belongsTo(db.catInstancias, {
+	foreignKey: 'idInstancia',
+	as: 'instancia'
+})
 
 module.exports = db
